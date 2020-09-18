c66fc959caaf022423f0487523fe1c4e2b90264a
SAS 826 <sasotillo826@gmail.com>
2020-09-18 00:06:38 -0400

    Updated HW

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,23 +1,37 @@
-// var sub = '940';
+function init() {
 
-d3.select('#selDataset').on("click", handleSelect);
-
-function handleSelect() {
-    let sub = d3.select('#selDataset').node().value
-    d3.event.preventDefault();
     d3.json('samples.json').then(data => {
         var names = data.names;
-        var demo = data.metadata.filter(obj => obj.id = sub)[0];
-        console.log(demo)
+        // var demo = data.metadata.filter(obj => obj.id = sub)[0] ;
+        // console.log(demo)
         // console.log(names);
         var select = d3.select('#selDataset');
-        var info = d3.select('#sample-metadata');
 
         d3.select("#selDataset").node().value = "";
         
         names.forEach(name => {
             select.append("option").text(name).property('value', name);
         });
+
+     
+    })
+    
+}
+
+init()
+
+d3.select('#selDataset').on("click", handleSelect);
+
+function handleSelect() {
+    let sub = d3.select('#selDataset').node().value
+
+    d3.event.preventDefault();
+    d3.json('samples.json').then(data => {
+        var demo = data.metadata.filter(obj => obj.id == sub)[0];
+        console.log(demo)
+        // var select = d3.select('#selDataset');
+        var info = d3.select('#sample-metadata');
+        info.html("")
 
         Object.entries(demo).forEach(([key, value]) => {
             info.append("h6").text(`${key.toUpperCase()}: ${value}`);
